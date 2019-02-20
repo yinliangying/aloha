440a8fb144b7a5ad41ecd8267b79b77d1bc27366
yinliangying <542260219@qq.com>
2019-02-20 13:05:29 +0800

    tensor2tensor 逆合成baseline: 字符级别

--- data/submit.py
+++ data/submit.py
@@ -3,12 +3,17 @@
 import sys
 import json
 fp_test_id=open("test_id")
-fp_test_predict=open()
-
+fp_test_predict=open("test_sample.transformer.transformer_tiny.my_reaction.beam4.alpha0.6.decodes")
+fp_result=open("result.json","w")
 
 out_dict={}
+i=0
 for line_id in fp_test_id:
+    i+=1
     id=line_id.strip()
     line_predict=fp_test_predict.readline().strip()
     out_dict[id]=line_predict
-print(json.dumps(out_dict))
+for line in fp_test_predict:
+    raise Exception
+print(i)
+print(json.dumps(out_dict),file=fp_result)
